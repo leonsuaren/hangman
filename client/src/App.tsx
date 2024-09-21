3c61ecc4c1bb0229782af8a8738a46b16f0be6bc
Leon Suarez <leonsua@gmail.com>
2024-09-21 18:37:27 -0400

    initial set-up, added first route and created main views folders

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import GameStart from "./views/game-start/GameStart.tsx";
+
+import "./App.css";
 
 function App() {
-
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<GameStart />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/views/game-start/GameStart.tsx
@@ -0,0 +1,9 @@
+import { type FC } from "react";
+
+const GameStart: FC = () => {
+  return (
+    <h1>Game Start</h1>
+  )
+};
+
+export default GameStart;
