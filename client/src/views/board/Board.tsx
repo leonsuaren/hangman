a33dc1b6f381ca65885e1e220250645b008835bd
Leon Suarez <leonsua@gmail.com>
2024-09-24 19:54:18 -0400

    guessed word logic and message logic working and tested on the 3 levels

--- client/src/views/board/Board.tsx
+++ client/src/views/board/Board.tsx
@@ -1,4 +1,4 @@
-import { type FC, type FormEvent, useRef, useState } from "react";
+import { type FC, type FormEvent, useRef, useState, useEffect } from "react";
 
 type BoardProps = {
   level: string;
@@ -9,34 +9,63 @@ const Board: FC<BoardProps> = ({ level, playedWord }) => {
   const [errors, setErrors] = useState<number>(0);
   const [letterExist, setLetterExist] = useState<number>(0);
   const [lettersSet, setLettersSet] = useState<string[]>([]);
+  const [maxLetterExist, setMaxLetterExist] = useState<number>(2);
   const [message, setMessage] = useState<string>("");
   const guessLetter = useRef<HTMLInputElement>(null);
   const playedWordGame = [...playedWord];
+
+  useEffect(() => {
+    if (level === "Medium") {
+      setMaxLetterExist(3);
+    }
+
+    if (level === "Difficult") {
+      setMaxLetterExist(4);
+    }
+
+    if (errors === 3) {
+      setMessage('You reached the maximum errors!');
+      setTimeout(() => {
+        setMessage('');
+      }, 3000);
+    }
+
+    if (letterExist === maxLetterExist) {
+      setMessage(`You reached the maximum number of correct answers ${maxLetterExist}!`);
+      setTimeout(() => {
+        setMessage('');
+      }, 3000);
+    }
+  }, [errors, letterExist]);
 
   const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     let letter = guessLetter.current?.value;
 
-    if (lettersSet.indexOf(letter!) < 0) {
+    //regla general para cualquier nivel de juego, easy, medium and difficult
+    if (lettersSet.indexOf(letter!) === -1) {
       setLettersSet((prevState) => [...prevState, letter!]);
     }
-    if (lettersSet.indexOf(letter!) >= 0) {
+    if (lettersSet.indexOf(letter!) !== -1) {
       setMessage("You entered that letter already");
       setTimeout(() => {
         setMessage("");
       }, 3000);
     }
+    //
+
     if (
-      playedWordGame.indexOf(letter!) >= 0 &&
-      lettersSet.indexOf(letter!) < 0
+      playedWordGame.indexOf(letter!) !== -1 &&
+      lettersSet.indexOf(letter!) === -1
     ) {
       setLetterExist((prevState) => prevState + 1);
     } else if (
       playedWordGame.indexOf(letter!) === -1 &&
-      lettersSet.indexOf(letter!) < 0
+      lettersSet.indexOf(letter!) === -1
     ) {
       setErrors((prevState) => prevState + 1);
     }
+
     event.currentTarget.reset();
   };
   console.log({
@@ -45,7 +74,7 @@ const Board: FC<BoardProps> = ({ level, playedWord }) => {
     Errors: errors,
     letterExist: letterExist,
     letterSet: lettersSet,
-    message: message
+    message: message,
   });
 
   return (
@@ -54,10 +83,14 @@ const Board: FC<BoardProps> = ({ level, playedWord }) => {
       <h2>Level {level}</h2>
       <section className="board-layout">
         <div className="restar-game">
-          <button><span>Restar Game</span></button>
+          <button>
+            <span>Restar Game</span>
+          </button>
         </div>
         <div className="reset-game">
-          <button><span>Reset Game</span></button>
+          <button>
+            <span>Reset Game</span>
+          </button>
         </div>
         <div className="guess-word">
           <h2>Guess Word</h2>
@@ -70,12 +103,20 @@ const Board: FC<BoardProps> = ({ level, playedWord }) => {
             ref={guessLetter}
             maxLength={1}
             disabled={
-              errors === 3 ? true : false || letterExist === 2 ? true : false
+              errors === 3
+                ? true
+                : false || letterExist === maxLetterExist
+                ? true
+                : false
             }
           />
           <button
             disabled={
-              errors === 3 ? true : false || letterExist === 2 ? true : false
+              errors === 3
+                ? true
+                : false || letterExist === maxLetterExist
+                ? true
+                : false
             }
           >
             Guess Letter
@@ -83,7 +124,7 @@ const Board: FC<BoardProps> = ({ level, playedWord }) => {
           <p>{message}</p>
         </form>
         <form className="guess-word-form">
-          <input type="text"/>
+          <input type="text" />
         </form>
         <div className="messages">
           <h2>Messages</h2>
